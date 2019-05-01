ab0e55f0176913833771e7a1d2e1c614b3886a92
Jeff <jmperkinson@gmail.com>
2019-05-01 14:42:15 -0400

    added partial header and footer

--- article.php
+++ article.php
@@ -2,50 +2,35 @@
 
 require 'partials/database.php';
 
-if (isset($_GET["id"]) && is_numeric($_GET["id"])){
+if (isset($_GET["id"]) && is_numeric($_GET["id"])) {
 
-$sql = "SELECT *
+    $sql = "SELECT *
         FROM articles
         WHERE id = " . $_GET["id"] . ";";
 
-$results = mysqli_query ($connection, $sql);
+    $results = mysqli_query($connection, $sql);
 
-if ($results === false) {
-echo mysqli_error ($connection) . nl2br ('<br>');
-echo mysqli_errno ($connection) . nl2br ('<br>');
-} else {
-$article = mysqli_fetch_assoc ($results);
-}
+    if ($results === false) {
+        echo mysqli_error($connection) . nl2br('<br>');
+        echo mysqli_errno($connection) . nl2br('<br>');
+    } else {
+        $article = mysqli_fetch_assoc($results);
+    }
 } else {
     $article = null;
 }
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<?php require 'partials/header.php'; ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Blog</title>
-</head>
+<?php if ($article === null) : ?>
+    <p>Article was not found.</p>
+<?php else : ?>
+    <article>
+        <h2><?= $article["title"]; ?></h2>
+        <p><?= $article["content"]; ?></p>
+    </article>
+<?php endif; ?>
 
-<body>
-    <header>
-        <h1>My Blog</h1>
-    </header>
-    <main>
-        <?php if ($article === null) : ?>
-            <p>Article was not found.</p>
-        <?php else : ?>
-            <article>
-                <h2><?= $article["title"]; ?></h2>
-                <p><?= $article["content"]; ?></p>
-            </article>
-        <?php endif; ?>
-    </main>
-</body>
-
-</html>
+<?php require 'partials/footer.php'; ?>
--- index.php
+++ index.php
@@ -17,36 +17,20 @@ if ($results === false) {
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<?php require 'partials/header.php'; ?>
+<?php if (empty($articles)) : ?>
+    <p>No articles found.</p>
+<?php else : ?>
+    <ul>
+        <?php foreach ($articles as $article) : ?>
+            <li>
+                <article>
+                    <h2><a href="article.php?id=<?= $article["id"] ?>"><?= $article["title"]; ?></a></h2>
+                    <p><?= $article["content"]; ?></p>
+                </article>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Blog</title>
-</head>
-
-<body>
-    <header>
-        <h1>My Blog</h1>
-    </header>
-    <main>
-        <?php if (empty($articles)) : ?>
-            <p>No articles found.</p>
-        <?php else : ?>
-            <ul>
-                <?php foreach ($articles as $article) : ?>
-                    <li>
-                        <article>
-                            <h2><a href="article.php?id=<?= $article["id"] ?>"><?= $article["title"]; ?></a></h2>
-                            <p><?= $article["content"]; ?></p>
-                        </article>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-        <?php endif; ?>
-    </main>
-</body>
-
-</html>
+<?php require 'partials/footer.php'; ?>
