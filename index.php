02956600f1c2dbcfede3571b4d7090f1a5b3b833
Jeff <jmperkinson@gmail.com>
2019-05-01 06:55:06 -0400

    added DB query of articles

--- index.php
+++ index.php
@@ -1,23 +1,36 @@
 <?php
 
-$articles = [
-    [
-        "title" => "First post",
-        "content" => "This is the first of many posts!"
-    ],
-    [
-        "title" => "Another Post",
-        "content" => "Yet another fascinating post..."
-    ],
-    [
-        "title" => "Read this!",
-        "content" => "You must read this now, it's essential reading!"
-    ],
-    [
-        "title" => "The Latest News",
-        "content" => "The latest news, read it now..."
-    ]
-]
+$db_host = "localhost";
+$db_name = "php_cms";
+$db_user = "cms_www";
+$db_password = "cn3WUv8WnxTwvzPo";
+
+$connection = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+
+if (!$connection) {
+    echo "Error: Unable to connect to MySQL." . nl2br('<br>');
+    echo "Debugging errno: " . mysqli_connect_errno() . nl2br('<br>'); //Error code
+    echo "Debugging error: " . mysqli_connect_error() . nl2br('<br>'); //Error text
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made!" . nl2br('<br>');
+echo "Host information: " . mysqli_get_host_info($connection) . nl2br('<br>');
+
+// mysqli_close($connection);
+
+$sql = "SELECT *
+        FROM articles
+        ORDER BY published_at;";
+
+$results = mysqli_query($connection, $sql);
+
+if ($results === false) {
+    echo mysqli_error($connection) . nl2br('<br>');
+    echo mysqli_errno($connection) . nl2br('<br>');
+} else {
+    $articles = mysqli_fetch_all($results, MYSQLI_ASSOC);
+}
 
 ?>
 
@@ -36,16 +49,20 @@ $articles = [
         <h1>My Blog</h1>
     </header>
     <main>
-        <ul>
-            <?php foreach ($articles as $article) : ?>
-                <li>
-                    <article>
-                        <h2><?= $article["title"]; ?></h2>
-                        <p><?= $article["content"]; ?></p>
-                    </article>
-                </li>
-            <?php endforeach; ?>
-        </ul>
+        <?php if (empty($articles)) : ?>
+            <p>No articles found.</p>
+        <?php else : ?>
+            <ul>
+                <?php foreach ($articles as $article) : ?>
+                    <li>
+                        <article>
+                            <h2><?= $article["title"]; ?></h2>
+                            <p><?= $article["content"]; ?></p>
+                        </article>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
     </main>
 </body>
 
